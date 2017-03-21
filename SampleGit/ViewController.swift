ed9b6d57494e6134603060f627e80995813eb7f4
Shruthi <shruthi.salian@robosoftin.com>
2017-03-21 14:08:04 +0530

    c2

--- SampleGit/ViewController.swift
+++ SampleGit/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //1st committ
+        //2nd committ
     }
 
     override func didReceiveMemoryWarning() {
