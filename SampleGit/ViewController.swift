595503400daf8747419b173ee2c90092a11e54c5
Shruthi <shruthi.salian@robosoftin.com>
2017-03-21 14:07:05 +0530

    Initial Commit

---
+++ SampleGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleGit
+//
+//  Created by Shruthi R Salian on 21/03/17.
+//  Copyright © 2017 Robosoft Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
